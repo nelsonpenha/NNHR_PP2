4d95c167571ffd644ba96d3c4b90d6fdb5fd1e9d
rociooviedo <npavon99@gmail.com>
2020-10-28 18:00:36 -0300

    test

--- tableros/tests/tests_models.py
+++ tableros/tests/tests_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from tableros.models import Tablero
-
+from tableros.models import Tarjeta
 
 class TestModels(TestCase):
 
@@ -12,4 +12,14 @@ class TestModels(TestCase):
             nombre='Tablero de Prueba',
             descripcion='Descripcion de prueba',
             estado='Iniciado'
+        )
+
+        self.card = Tarjeta.objects.create(
+            id_tarjeta=1,
+            nombre_tarjeta=' rueba',
+            descripcion='Descripcion de prueba',
+            fecha_limite='2020-09-19',
+            id_usuario='1',
+            id_tablero='5',
+            estado='Activo'
         )
